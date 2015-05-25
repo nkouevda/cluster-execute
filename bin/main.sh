0e59a744ccc2317c174f5bee6cac503bd772e134
Nikita Kouevda <nkouevda@gmail.com>
2015-05-24 17:51:18 -0700

    Collapse subshell into single background command

--- bin/main.sh
+++ bin/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Nikita Kouevda
-# 2015/05/18
+# 2015/05/24
 
 # Switch to parent directory of location of script
 cd "$(dirname "$BASH_SOURCE")/.."
@@ -22,17 +22,13 @@ mkdir -p "$stdout_dir" "$stderr_dir"
 # Print the full path to the base output directory for this process
 echo ~/"$remote_dir/$output_dir/$$/"
 
-# Run remote script on each server in a background subshell
+# Run remote script on each server in background for concurrent execution
 sed -n '/^[^#]/p' "$server_list" | while read -r server; do
-  (
-    # Retrieve and write output
-    ssh "${ssh_config[@]}" "$username@$server" \
-        "$remote_dir/$task_script $server" >"$stdout_dir/$server" \
-        2>"$stderr_dir/$server"
-
-    # Record the server as offline if ssh returned nonzero
-    (( $? )) && echo "$server" >>"$offline_servers"
-  ) &
+  # Retrieve and write output; record server as offline if ssh returns nonzero
+  ssh "${ssh_config[@]}" "$username@$server" \
+      "$remote_dir/$task_script $server" >"$stdout_dir/$server" \
+      2>"$stderr_dir/$server" \
+      || echo "$server" >>"$offline_servers" &
 done
 
 # Wait for all background jobs to finish
